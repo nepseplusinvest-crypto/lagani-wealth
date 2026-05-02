5786dfe594c9db83683afcc1c7a5d5c93478817a
nepseplusinvest-crypto <nepseplusinvest@gmail.com>
2026-05-02 14:41:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
-let globalData = [];
+function loadHistory(){
+  let date = document.getElementById("datePick").value;
 
-fetch('./data.json')
-.then(res => res.json())
-.then(d => {
-  globalData = d.all || [];
-
-  render("gainers", d.gainers);
-  render("losers", d.losers);
-  render("all", d.all);
-
-  document.getElementById("status").innerText =
-    "Last updated: " + new Date().toLocaleString();
-})
-.catch(err => {
-  document.getElementById("status").innerText =
-    "Using cached data...";
-});
-
-function render(id, list){
-  let html = "";
-
-  if(!list || list.length === 0){
-    document.getElementById(id).innerHTML = "No data";
-    return;
-  }
-
-  list.forEach(s => {
-    html += `
-      <div class="stock">
-        <span>${s.symbol}</span>
-        <span>${s.price} (${s.change}%)</span>
-      </div>
-    `;
-  });
-
-  document.getElementById(id).innerHTML = html;
+  fetch(`./history-${date}.json`)
+  .then(res => res.json())
+  .then(d => {
+    render("all", d.all);
+    render("gainers", d.gainers);
+    render("losers", d.losers);
+  })
+  .catch(() => alert("No data for this date"));
 }
